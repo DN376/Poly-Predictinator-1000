9bbf92e5cd5e30e989b6ade7a2c570c0d16cab49
DN376 <dave030706@gmail.com>
2023-09-16 14:19:18 -0400

    Added Options & Modularized App.py
    Added options for the user to select how many articles to summarize.
    
    Also modularized the program, putting each major step into its own function for cleanliness.

--- app.py
+++ app.py
@@ -12,25 +12,51 @@ def main():
     st.set_page_config(page_title="Quick Report",
                        page_icon=":rolled_up_newspaper:")
     st.header("Quick Report :rolled_up_newspaper:")
+    google_news = GNews()
 
     subject = st.text_input('**Choose a Subject to Summarize:**')
 
     st.write("Subject: " + subject)
 
-    with st.spinner("Removing Stopwords..."):
-        cleanSubj = removeStops(subject)
-    st.write("clean = " + cleanSubj)
-    with st.spinner("Getting articles..."):
-        google_news = GNews()
-        news = google_news.get_news(cleanSubj)
+    cleanSubj = removeStops(subject)
+
+    st.write("Subject: " + cleanSubj)# for debugging purposes ONLY
+
+    news = getArticles(cleanSubj, google_news)
+
     st.write("Found " + str(len(news)) + " articles")
-    news1 = [news[0]]
-    for article in news1:
-        article_text = google_news.get_full_article(article['url']).text
-        st.write(article_text)
+    
+    newsSelection = presentOptions(news)
+
+    displayText(newsSelection, google_news)
 
 def removeStops(subject):
-    cachedStopWords = stopwords.words("english")
-    return ' '.join([word for word in subject.split() if word not in cachedStopWords])
+    with st.spinner("Removing Stopwords..."):
+        cachedStopWords = stopwords.words("english")
+        return ' '.join([word for word in subject.split() if word not in cachedStopWords])
+
+def getArticles(cleanSubj, google_news):
+    with st.spinner("Getting articles..."):
+        news = google_news.get_news(cleanSubj)
+    return news
+
+def presentOptions(news):
+    if(st.button("Main Article")):
+        newsSelection = [news[0]]
+    if(st.button("Top 5 Articles")):
+        newsSelection = news[0:5]
+    if(st.button("Top 10 Articles")):
+        newsSelection = news[0:10]
+    if(st.button("Top 25 Articles")):
+        newsSelection = news[0:25]
+    if(st.button("All Articles")):
+        newsSelection = news
+    return newsSelection
+
+def displayText(newsSelection, google_news):
+    for article in newsSelection:
+        article_text = google_news.get_full_article(article['url']).title
+        st.write(article_text)
+        st.write("------\n")
 
 main()
