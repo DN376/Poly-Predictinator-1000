f4001b00caf626f3d6949e101b2a7d1208560c39
DN376 <dave030706@gmail.com>
2023-09-16 16:11:54 -0400

    Fixed "None" issue
    Fixed a bug where some articles would be a None object when trying to access their text, leading to a crash in the program.
    
    This was done by fixing the way the top articles were selected, filtering out the ones that were None.

--- app.py
+++ app.py
@@ -20,13 +20,11 @@ def main():
 
     cleanSubj = removeStops(subject)
 
-    st.write("Subject: " + cleanSubj)# for debugging purposes ONLY
-
     news = getArticles(cleanSubj, google_news)
 
     st.write("Found " + str(len(news)) + " articles")
     
-    newsSelection = presentOptions(news)
+    newsSelection = presentOptions(news, google_news)
 
     displayText(newsSelection, google_news)
 
@@ -40,25 +38,43 @@ def getArticles(cleanSubj, google_news):
         news = google_news.get_news(cleanSubj)
     return news
 
-def presentOptions(news):
+def presentOptions(news, google_news):
     if(st.button("Main Article")):
-        newsSelection = [news[0]]
+        newsSelection = getTopXArticles(1, news, google_news)
     if(st.button("Top 5 Articles")):
-        newsSelection = news[0:5]
+        newsSelection = getTopXArticles(5, news, google_news)
     if(st.button("Top 10 Articles")):
-        newsSelection = news[0:10]
+        newsSelection = getTopXArticles(10, news, google_news)
     if(st.button("Top 25 Articles")):
-        newsSelection = news[0:25]
-    if(st.button("All Articles")):
-        newsSelection = news
+        newsSelection = getTopXArticles(25, news, google_news)
+    if(st.button("Top 50 Articles")):
+        newsSelection = getTopXArticles(50, news, google_news)
     return newsSelection
 
+def getTopXArticles(x, news, google_news):
+    newsSelection = []
+    debug = 0
+    for article in news:
+        full_article = google_news.get_full_article(article['url'])
+        st.write(full_article) #DEBUGGING
+        if full_article is not None:
+            newsSelection.append(article)
+            x -= 1
+        else:
+            debug += 1
+        if x == 0:
+            st.write("debug = " + str(debug)) # DEBUGGING
+            return newsSelection
+
+
 def displayText(newsSelection, google_news):
-    st.write(newsSelection)
+    st.write(newsSelection) # DEBUGGING
     i = 1
     for article in newsSelection:
         with st.spinner("Acessing file #" + str(i)):
             st.write("**File #" + str(i) + ":**")
+            full_article = google_news.get_full_article(article['url'])
+            st.write(full_article) # DEBUGGING
             article_text = google_news.get_full_article(article['url']).title
             st.write(article_text)
             st.write("------\n")
