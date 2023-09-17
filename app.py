200aab86692246df2e0593d9c8e755fc8fdf81a1
DN376 <dave030706@gmail.com>
2023-09-17 00:27:24 -0400

    Update App.py
    Used .env variables instead of having Cohere API key in public (security issue).
    
    Tweaked Cohere summarization AI to be faster and incorporate more connections from all articles.
    
    Added a summary of all summarized articles at the end of the page.
    
    Improved Article filtering system to ensure that the program is bug-free.
    
    Polished printing of summaries to stop turning text into Latex.
    
    Stopped QoL issue where streamlit would display errors due to the order in which commands were run.

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import datetime
 import nltk
 from nltk.corpus import stopwords
 import cohere
+from dotenv import load_dotenv
+import os
 nltk.download('stopwords')
+COHERE_API_KEY = os.getenv('COHERE_API_KEY')
 
 
 def main():
-
+    load_dotenv()
     st.set_page_config(page_title="Quick Report",
                        page_icon=":rolled_up_newspaper:")
     st.header("Quick Report :rolled_up_newspaper:")
@@ -26,7 +29,8 @@ def main():
         
         newsSelection = presentOptions(news, google_news)
 
-        displayText(newsSelection, google_news)
+        if newsSelection is not None:
+            displayText(newsSelection, google_news)
 
 def removeStops(subject):
     with st.spinner("Processing Input..."):
@@ -39,25 +43,33 @@ def getArticles(cleanSubj, google_news, subject):
     return news
 
 def presentOptions(news, google_news):
+    pressed = False
     if(st.button("Main Article")):
         newsSelection = getTopXArticles(1, news, google_news)
+        pressed = True 
     if(st.button("Top 5 Articles")):
         newsSelection = getTopXArticles(5, news, google_news)
+        pressed = True 
     if(st.button("Top 10 Articles")):
         newsSelection = getTopXArticles(10, news, google_news)
+        pressed = True 
     if(st.button("Top 25 Articles")):
         newsSelection = getTopXArticles(25, news, google_news)
+        pressed = True 
     if(st.button("Top 50 Articles")):
         newsSelection = getTopXArticles(50, news, google_news)
-    return newsSelection
+        pressed = True 
+    if pressed:
+        return newsSelection
 
 def getTopXArticles(x, news, google_news):
     newsSelection = []
     for article in news:
         full_article = google_news.get_full_article(article['url'])
         if full_article is not None:
-            newsSelection.append(article)
-            x -= 1
+            if len(full_article.text) > 250:
+                newsSelection.append(article)
+                x -= 1
         if x == 0:
             return newsSelection
 
@@ -70,24 +82,30 @@ def displayText(newsSelection, google_news):
             st.write("**Article #" + str(i) + ": "+ article['title'] +"**")
             article_text = google_news.get_full_article(article['url']).text
             summary = summarizeText(article_text, 'medium')
-            st.write(summary)
+            summaryProcessed = summary.replace("$", "\$")
+            st.write(summaryProcessed)
             st.write("------\n")
             newsSummary += summary
             i += 1
     if(i > 2):
         st.write("**In Summary:**")
-        newsSummary
+        with st.spinner("Forming Summary of Topic..."):
+            print(newsSummary)
+            fullSum = summarizeText(newsSummary,'long')
+            fullProcessed = fullSum.replace("$","\$")
+            st.write(fullProcessed)
     
 
 def summarizeText(article, len):
-    co = cohere.Client('WYbq1RzI3MniGl2xfq6iG0vKC6JeIPUptalaJwcZ')
+    co = cohere.Client(COHERE_API_KEY)
     response = co.summarize(
         text=article,
-        model = 'command-nightly',
+        model = 'command-light-nightly',
         temperature = 0,
         length = len,
         format = 'paragraph',
-        extractiveness = 'low'
+        extractiveness = 'low',
+        additional_command = "making sure to incorporate elements from all parts of the article and trying to connect them together"
     )
     return response.summary
 
